49a3082c9387bf5995855fb3b87b9c836eac2a61
houami <vigneshwarv@gmail.com>
2017-10-18 15:06:42 -0400

    ec2 with tags

--- ec2_primer/main.tf
+++ ec2_primer/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
 
