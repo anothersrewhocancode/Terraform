dd197f5c0aa6ff33dcae6b6774ae047c9a2a4942
Vignesh <vigneshwarv@gmail.com>
2019-10-01 10:39:27 +0530

    adds vpc setups

---
+++ aws/vpcs/two_tier_vpc/main.tf
@@ -0,0 +1,108 @@
+resource "aws_vpc" "main" {
+    cidr_block = "${var.cidr_block}"
+    instance_tenancy = "${var.instance_tenancy}"
+    enable_dns_support = "${var.enable_dns_support}"
+    enable_dns_hostnames = "${var.enable_dns_hostnames}"
+    tags = {
+      Name = "main"
+    }
+}
+
+
+# Subnets
+resource "aws_subnet" "main-public-1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.pubsubnet1_cidr_block}"
+    map_public_ip_on_launch = "${var.pubsub_map_public_ip_on_launch}"
+    availability_zone = "${var.subnet1_availability_zone}"
+
+    tags = {
+        Name = "main-public-1"
+    }
+}
+resource "aws_subnet" "main-public-2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.pubsubnet2_cidr_block}"
+    map_public_ip_on_launch = "${var.pubsub_map_public_ip_on_launch}"
+    availability_zone = "${var.subnet2_availability_zone}"
+
+    tags = {
+        Name = "main-public-2"
+    }
+}
+resource "aws_subnet" "main-public-3" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.pubsubnet3_cidr_block}"
+    map_public_ip_on_launch = "${var.pubsub_map_public_ip_on_launch}"
+    availability_zone = "${var.subnet3_availability_zone}"
+
+    tags = {
+        Name = "main-public-3"
+    }
+}
+resource "aws_subnet" "main-private-1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.prisubnet1_cidr_block}"
+    map_public_ip_on_launch = "${var.prisub_map_public_ip_on_launch}"
+    availability_zone = "${var.subnet1_availability_zone}"
+
+    tags = {
+        Name = "main-private-1"
+    }
+}
+resource "aws_subnet" "main-private-2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block =  "${var.prisubnet2_cidr_block}"
+    map_public_ip_on_launch = "${var.prisub_map_public_ip_on_launch}"
+    availability_zone = "${var.subnet2_availability_zone}"
+
+    tags = {
+        Name = "main-private-2"
+    }
+}
+resource "aws_subnet" "main-private-3" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.prisubnet3_cidr_block}"
+    map_public_ip_on_launch = "${var.prisub_map_public_ip_on_launch}"
+    availability_zone = "${var.subnet3_availability_zone}"
+
+    tags = {
+        Name = "main-private-3"
+    }
+}
+
+# Internet GW
+resource "aws_internet_gateway" "main-gw" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags = {
+        Name = "main"
+    }
+}
+
+# route tables
+resource "aws_route_table" "main-public" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.main-gw.id}"
+    }
+
+    tags = {
+        Name = "main-public-1"
+    }
+}
+
+# route associations public
+resource "aws_route_table_association" "main-public-1-a" {
+    subnet_id = "${aws_subnet.main-public-1.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+resource "aws_route_table_association" "main-public-2-a" {
+    subnet_id = "${aws_subnet.main-public-2.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+resource "aws_route_table_association" "main-public-3-a" {
+    subnet_id = "${aws_subnet.main-public-3.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
---
+++ aws/vpcs/two_tier_vpc/variables.tf
@@ -0,0 +1,72 @@
+variable "cidr_block" {
+  description = "vpc cidr block"
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  description = "tenancy of the instance"
+  default     = "default"
+}
+
+variable "enable_dns_support" {
+  description = "provides dns support "
+  default     = "true"
+}
+
+variable "enable_dns_hostnames" {
+  description = "provides dns hostnames"
+  default     = "true"
+}
+
+variable "pubsubnet1_cidr_block" {
+  description = "cidr of the public subnet1"
+  default     = "10.0.1.0/24"
+}
+
+variable "pubsubnet2_cidr_block" {
+  description = "cidr of the public subnet2"
+  default     = "10.0.2.0/24"
+}
+
+variable "pubsubnet3_cidr_block" {
+  description = "cidr of the public subnet3"
+  default     = "10.0.3.0/24"
+}
+
+variable "prisubnet1_cidr_block" {
+  description = "cidr of private subnet1"
+  default     = "10.0.4.0/24"
+}
+variable "prisubnet2_cidr_block" {
+  description = "cidr of private subnet2"
+  default     = "10.0.5.0/24"
+}
+variable "prisubnet3_cidr_block" {
+  description = "cidr of private subnet3"
+  default     = "10.0.6.0/24"
+}
+
+variable "pubsub_map_public_ip_on_launch" {
+  description = "enable public ip"
+  default     = "true"
+}
+
+variable "prisub_map_public_ip_on_launch" {
+  description = "enable public ip"
+  default     = "false"
+}
+
+variable "subnet1_availability_zone" {
+  description = "availability_zone of the subnets"
+  default     = "eu-west-2a"
+}
+
+variable "subnet2_availability_zone" {
+  description = "availability_zone of the subnets"
+  default     = "eu-west-2b"
+}
+
+variable "subnet3_availability_zone" {
+  description = "availability_zone of the subnets"
+  default     = "eu-west-2c"
+}
